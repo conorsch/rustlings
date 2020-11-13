f1283e106e7f6a017a29b03d2a66fcc284608eb3
Conor Schaefer <conor@freedom.press>
2020-11-13 15:16:17 -0800

    tests2

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,13 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let var1: u8 = 1;
+        let var2: u8 = 1;
+
+        assert_eq!(var1, var2);
     }
 }
